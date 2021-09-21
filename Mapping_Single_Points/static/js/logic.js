7eb240bb2ddcd4f5fe0ba7193f166880fda69d86
kwporras <k.porras@yahoo.com>
2021-09-20 20:44:40 -0500

    update logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -14,8 +14,25 @@ console.log("working");
 // Create the map object with options.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle to the map for Los Angeles, California.
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+// Add a circle to the map for Los Angeles, California using the circleMarker() function.
+ L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+    }).addTo(map);
+
+
+
 //Create Tile Layer for the Map that will be the background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
