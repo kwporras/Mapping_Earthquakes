2243418ca849c909ce476de9391f423ee004eebf
kwporras <k.porras@yahoo.com>
2021-09-22 17:23:57 -0500

    upload files

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -14,21 +14,33 @@ console.log("working");
 // Create the map object with options.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Get data from cities.js
+let cityData = cities;
 
-// Add a circle to the map for Los Angeles, California.
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange"
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
+
+
+// // Add a circle to the map for Los Angeles, California.
 // L.circle([34.0522, -118.2437], {
 //     radius: 100
 //  }).addTo(map);
 
-// Add a circle to the map for Los Angeles, California using the circleMarker() function.
- L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
-    }).addTo(map);
-
+// // Add a circle to the map for Los Angeles, California using the circleMarker() function.
+//  L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+//     }).addTo(map);
 
 
 //Create Tile Layer for the Map that will be the background of the map
