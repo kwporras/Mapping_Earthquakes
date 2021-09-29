6fc46dbd1232a16aa831a4baf99efcb0bce945f7
kwporras <k.porras@yahoo.com>
2021-09-29 14:22:53 -0500

    clean up code

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,6 +1,13 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
+// To change the map's style, change the map id using the list of Mapbox ids below:
+// mapbox/streets-v11
+// mapbox/outdoors-v11
+// mapbox/light-v10
+// mapbox/dark-v10
+// mapbox/satellite-v9
+// mapbox/satellite-streets-v11
 
 //Create Tile Layer for the Map that will be the background of the map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -15,14 +22,6 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
     maxZoom: 18,
     accessToken: API_KEY
 });
-
-// To change the map's style, change the map id using the list of Mapbox ids below:
-// mapbox/streets-v11
-// mapbox/outdoors-v11
-// mapbox/light-v10
-// mapbox/dark-v10
-// mapbox/satellite-v9
-// mapbox/satellite-streets-v11
 
 // Create a base layer that holds both maps.
 let baseMaps = {
