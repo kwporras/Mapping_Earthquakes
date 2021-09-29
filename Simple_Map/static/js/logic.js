a97f2a6a632b451b698f2206ef79b22e58ca0f69
kwporras <k.porras@yahoo.com>
2021-09-29 14:12:35 -0500

    clean up code

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -14,15 +14,6 @@ console.log("working");
 // Create the map object with options.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//Create Tile Layer for the Map that will be the background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
-// Add the 'streets' tile layer to the map
-streets.addTo(map);
-
 // To change the map's style, change the map id using the list of Mapbox ids below:
 // mapbox/streets-v11
 // mapbox/outdoors-v11
@@ -31,4 +22,12 @@ streets.addTo(map);
 // mapbox/satellite-v9
 // mapbox/satellite-streets-v11
 
+//Create Tile Layer for the Map that will be the background of the map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
 
+// Add the 'streets' tile layer to the map
+streets.addTo(map);
